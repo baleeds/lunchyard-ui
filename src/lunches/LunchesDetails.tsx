bac2f64e1d5e5d17bcd424f11b97a69ad92d4701
Benjamin Leeds <baleeds@ncsu.edu>
2019-10-02 11:02:04 -0400

    Added a placeholder component

--- src/lunches/LunchesDetails.tsx
+++ src/lunches/LunchesDetails.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useRouter } from '../router';
+import Placeholder from '../shared/Placeholder';
+
+import { ReactComponent as TableIcon } from '../shared/icons/table.svg';
 
 const LunchDetails: React.FC = () => {
   const { params } = useRouter();
   const { lunchId } = params;
 
-  if (!lunchId) return null;
+  if (!lunchId) return <Placeholder Icon={TableIcon} message="select a lunch" />;
   
   return (
     <div>
