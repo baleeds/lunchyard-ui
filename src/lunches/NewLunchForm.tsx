1b8c69cedd7eba1751b5478bb9451f78fcb6d0b2
Benjamin Leeds <baleeds@ncsu.edu>
2019-10-04 11:45:35 -0400

    Move createLunchMutation into its own file

--- src/lunches/NewLunchForm.tsx
+++ src/lunches/NewLunchForm.tsx
@@ -6,10 +6,7 @@ import FormGroup from '../shared/form/FormGroup';
 import { ButtonPrimary, ButtonGhost } from '../shared/html/Buttons';
 import theme from '../constants/theme';
 import { ReactComponent as CheckIcon } from '../shared/icons/check.svg';
-import { useMutation } from '@apollo/react-hooks';
-import createLunchMutation, { CreateLunchMutationVariables, CreateLunchMutationData } from './mutations/createLunchMutation';
-import lunchesQuery, { LunchesQueryData, LunchesQueryVariables } from './queries/lunchesQuery';
-import CacheQuery from '../apollo/CacheQuery';
+import useCreateLunchMutation from './mutations/createLunchMutation';
 
 const NewLunchForm: React.FC = () => {
   const navigate = useNavigate();
@@ -18,21 +15,7 @@ const NewLunchForm: React.FC = () => {
 
   const closeForm = useCallback(() => navigate(routes.lunches.getPath()), [navigate]);
 
-  // QUESTION: The types for the update function are inferred from the useMutation types.
-  // If I extract that function I lose the inference?
-  const [handleCreateLunch, { loading }] = useMutation<CreateLunchMutationData, CreateLunchMutationVariables>(createLunchMutation, {
-    onCompleted: ({ createLunch }) => { navigate(routes.lunchDetails.getPath({ lunchId: createLunch.id })); },
-    update: (cache, { data }) => {
-      if (!data || !data.createLunch) return;
-
-      const lunchesCacheQuery = new CacheQuery<LunchesQueryData, LunchesQueryVariables>(cache, {
-        query: lunchesQuery,
-        variables: { first: 100 },
-      });
-
-      lunchesCacheQuery.addEdge('lunches', {node: data.createLunch, __typename: 'LunchEdge '});
-    },
-  });
+  const [handleCreateLunch, { loading }] = useCreateLunchMutation();
 
   const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
--- src/lunches/mutations/createLunchMutation.ts
+++ src/lunches/mutations/createLunchMutation.ts
@@ -1,5 +1,10 @@
 import gql from "graphql-tag";
 import lunchDetailsFragment from "../fragments/lunchDetailsFragment";
+import { useMutation } from "@apollo/react-hooks";
+import { useNavigate } from "../../router";
+import routes from "../../constants/routes";
+import CacheQuery from "../../apollo/CacheQuery";
+import lunchesQuery, { LunchesQueryData, LunchesQueryVariables } from "../queries/lunchesQuery";
 
 export interface CreateLunchMutationVariables {
   occasion: string,
@@ -18,4 +23,24 @@ const createLunchMutation = gql`
   ${lunchDetailsFragment}
 `;
 
-export default createLunchMutation;
+// QUESTION: The types for the update function are inferred from the useMutation types.
+// If I extract that function I lose the inference?
+const useCreateLunchMutation = () => {
+  const navigate = useNavigate();
+  
+  return useMutation<CreateLunchMutationData, CreateLunchMutationVariables>(createLunchMutation, {
+    onCompleted: ({ createLunch }) => { navigate(routes.lunchDetails.getPath({ lunchId: createLunch.id })); },
+    update: (cache, { data }) => {
+      if (!data || !data.createLunch) return;
+
+      const lunchesCacheQuery = new CacheQuery<LunchesQueryData, LunchesQueryVariables>(cache, {
+        query: lunchesQuery,
+        variables: { first: 100 },
+      });
+
+      lunchesCacheQuery.addEdge('lunches', {node: data.createLunch, __typename: 'LunchEdge '});
+    },
+  });
+}
+
+export default useCreateLunchMutation;
