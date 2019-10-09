28ea328eba1aee86ecd67b6b38fb57df8cb7791a
Benjamin Leeds <baleeds@ncsu.edu>
2019-10-09 10:56:54 -0400

    Allow for mofule toolbar to get data

--- src/components/lunches/Lunches.tsx
+++ src/components/lunches/Lunches.tsx
@@ -1,35 +1,17 @@
-import React, { useMemo } from 'react';
-import ModuleToolbar from '../common/ModuleToolbar';
-import { ReactComponent as PlusIcon } from '../common/icons/add.svg';
+import React from 'react';
 import { ReactComponent as TableIcon } from '../common/icons/table.svg';
 import LunchDetails from './details/LunchesDetails';
 import LunchesList from './LunchesList';
-import { useNavigate, useRouter } from '../../lib/router';
-import routes from '../../constants/routes';
+import { useRouter } from '../../lib/router';
 import Placeholder from '../common/Placeholder';
-import ListContainer from '../common/ListContainer';
 import DetailsContainer from '../common/DetailsContainer';
 
 const Lunches: React.FC = () => {
-  const navigate = useNavigate();
   const { params: { lunchId } } = useRouter();
-
-  const moduleToolbarButton = useMemo(() => ({
-    title: 'create lunch',
-    onClick: () => navigate(routes.lunchCreate.getPath()),
-    Icon: PlusIcon,
-  }), [navigate]);
 
   return (
     <>
-      <ListContainer>
-        <ModuleToolbar
-          title="lunches"
-          subTitle="3 upcoming"
-          button={moduleToolbarButton}
-        />
-        <LunchesList />
-      </ListContainer>
+      <LunchesList />
       <DetailsContainer>
         {lunchId
           ? <LunchDetails key={`${lunchId}-lunchDetails`} lunchId={lunchId} /> 
--- src/components/lunches/LunchesList.tsx
+++ src/components/lunches/LunchesList.tsx
@@ -1,19 +1,29 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import routes from '../../constants/routes';
 import List from '../common/List';
 import LunchesListItem from './LunchesListItem';
-import { useRouter } from '../../lib/router';
+import { useRouter, useNavigate } from '../../lib/router';
 import NewLunchForm from './NewLunchForm';
 import { useLunchOptionsQuery, LunchOptionFragment } from '../../api/types';
+import { ReactComponent as PlusIcon } from '../common/icons/add.svg';
 import { ReactComponent as Plate } from '../common/icons/plate.svg';
 import Placeholder from '../common/Placeholder';
+import ListContainer from '../common/ListContainer';
+import ModuleToolbar from '../common/ModuleToolbar';
 
 const getPathFromLunchItem = (item: LunchOptionFragment) => routes.lunchDetails.getPath({ lunchId: item.id });
 
 const LunchesList: React.FC = () => {
+  const navigate = useNavigate();
   const { id, params: { lunchId } } = useRouter();
 
   const getIsActive = useCallback((item: LunchOptionFragment) => item.id === lunchId, [lunchId]);
+
+  const moduleToolbarButton = useMemo(() => ({
+    title: 'create lunch',
+    onClick: () => navigate(routes.lunchCreate.getPath()),
+    Icon: PlusIcon,
+  }), [navigate]);
 
   const showCreate = id === routes.lunchCreate.id;
   
@@ -23,19 +33,34 @@ const LunchesList: React.FC = () => {
     },
   });
 
-  if (loading) return null;
-  if (!data) return <Placeholder Icon={Plate} message="We're having trouble loading lunches" />;
+  const { lunches } = data || {};
+
+  const renderList = useCallback(() => {
+    if (loading) return null;
+    if (!lunches) return <Placeholder Icon={Plate} message="We're having trouble loading lunches" />;
+    
+    return (
+      <List<LunchOptionFragment>
+        items={lunches.edges.map(edge => edge.node)}
+        ListItem={LunchesListItem}
+        getPath={getPathFromLunchItem}
+        getIsActive={getIsActive}
+        showCreate={showCreate}
+        CreatableForm={<NewLunchForm />}
+      />
+    );
+  }, [loading, lunches, getIsActive, showCreate]);
 
   return (
-    <List<LunchOptionFragment>
-      items={data.lunches.edges.map(edge => edge.node)}
-      ListItem={LunchesListItem}
-      getPath={getPathFromLunchItem}
-      getIsActive={getIsActive}
-      showCreate={showCreate}
-      CreatableForm={<NewLunchForm />}
-    />
-  )
+    <ListContainer>
+      <ModuleToolbar
+        title="lunches"
+        subTitle={lunches ? `${lunches.edges.length} upcoming` : undefined}
+        button={moduleToolbarButton}
+      />
+      {renderList()}
+    </ListContainer>
+  );
 };
 
 export default LunchesList;
