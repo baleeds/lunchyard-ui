28e95d25674d5a0b3d378e86dcdf89a726c01820
Benjamin Leeds <baleeds@ncsu.edu>
2019-10-09 13:19:07 -0400

    Refactor vendors list to be new generic list component

--- src/components/vendors/VendorsList.tsx
+++ src/components/vendors/VendorsList.tsx
@@ -1,42 +1,33 @@
-import React, { useCallback } from 'react';
-import prune from '../../lib/apollo/prune';
+import React from 'react';
 import routes from '../../constants/routes';
-import List from '../common/List';
 import VendorsListItem from './VendorsListItem';
-import { useRouter } from '../../lib/router';
 import NewVendorForm from './NewVendorForm';
-import { useVendorsQuery, Vendor } from '../../api/types';
+import { Vendor, VendorOptionFragment, VendorOptionsQuery, VendorOptionsQueryVariables, useVendorOptionsQuery } from '../../api/types';
+import ModuleList from '../common/ModuleList';
+import { ReactComponent as RestaurantIcon } from '../common/icons/store.svg';
 
 const getPathFromVendorItem = (item: Vendor) => routes.vendorDetails.getPath({ vendorId: item.id });
+const getConnectionFromData = (data: VendorOptionsQuery | undefined) => data ? data.vendors : undefined;
+const getIdFromParams = (params: any) => params.vendorId;
 
 const VendorsList: React.FC = () => {
-  const { id, params: { vendorId } } = useRouter();
-
-  const getIsActive = useCallback((item: Vendor) => item.id === vendorId, [vendorId]);
-
-  const showCreate = id === routes.vendorCreate.id;
-  
-  const { data, loading, error } = useVendorsQuery({
-    variables: {
-      first: 100,
-    },
-  });
-
-  if (loading) return <span>loading</span>;
-  if (error || !data) return <span>error</span>;
-
-  const vendors = prune(data.vendors);
-
   return (
-    <List<Vendor>
-      items={vendors}
-      ListItem={VendorsListItem}
-      getPath={getPathFromVendorItem}
-      getIsActive={getIsActive}
-      showCreate={showCreate}
-      CreatableForm={<NewVendorForm />}
+    <ModuleList<VendorOptionFragment, VendorOptionsQuery, VendorOptionsQueryVariables>
+      ModuleIcon={RestaurantIcon}
+      createButtonTitle="create restaurant"
+      createRoute={routes.vendorCreate}
+      getConnectionFromData={getConnectionFromData}
+      getIdFromParams={getIdFromParams}
+      listProps={{
+        CreatableForm: <NewVendorForm />,
+        ListItem: VendorsListItem,
+        getPath: getPathFromVendorItem, 
+      }}
+      listTitle="restaurants"
+      optionsQueryHook={useVendorOptionsQuery}
+      optionsQueryVariables={{ first: 100 }}
     />
-  )
+  );
 };
 
 export default VendorsList;
