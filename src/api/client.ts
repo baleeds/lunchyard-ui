e383cf99c41f7026e6b8f42cd2b7a6b6de8e0321
Benjamin Leeds <baleeds@ncsu.edu>
2019-10-11 08:59:34 -0400

    Convert to real apollo client

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,24 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: ApolloLink.from([
+    // watchedMutationLink,
+    httpLink,
+  ]),
+  cache: new InMemoryCache({
+    cacheRedirects: {
+      Query: {
+        node: (_, args, { getCacheKey }) => getCacheKey({ id: args.id }),
+      },
+    },
+  }),
 });
 
 // @ts-ignore
